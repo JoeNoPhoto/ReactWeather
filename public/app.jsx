7e33cc9ad347586afedd1570d82526776f6bc6ee
Joe Norris <yourboybstar@gmail.com>
2016-06-14 15:56:33 -0700

    Initial Commit of Greeter

---
+++ public/app.jsx
@@ -0,0 +1,44 @@
+var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'React',
+      message: 'This is a default message',
+    };
+  },
+  getInitialState: function(){
+    return{
+      name: this.props.name
+    };
+  },
+  onButtonClick: function(e) {
+    e.preventDefault();
+
+    var nameRefs = this.refs.name;
+    var name = nameRefs.value;
+    nameRefs.value = '';
+
+    if(typeof name === 'string' && name.length > 0) {
+      this.setState({
+        name: name
+      });
+    }
+  },
+  render: function() {
+    var name = this.state.name;
+    var message = this.props.message;
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
+      <form onSubmit={this.onButtonClick}>
+        <input type="text" ref="name" />
+      <button> Set Name</button>
+      </form>
+      </div>
+    );
+  }
+});
+
+var firstName = 'Joe';
+
+ReactDOM.render(<Greeter name={firstName} />,document.getElementById('app'));
