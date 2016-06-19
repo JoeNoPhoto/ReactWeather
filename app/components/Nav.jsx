c7098e4c4606a7b86faa731fedf4c49fe0ef67ca
Joe Norris <yourboybstar@gmail.com>
2016-06-19 15:26:59 -0700

    Added Nav Search support

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,14 +4,23 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass ({
   onSearch: function(e) {
     e.preventDefault();
-    alert('Not yet wired up!');
+
+    var location = this.refs.search.value;
+    var encodedLocation = encodeURIComponent(location);
+
+    if(location.length > 0) {
+      location = '';
+      window.location.hash = '#/?location=' + encodedLocation;
+    }
+
+
   },
   render: function () {
     return(
       <div className="top-bar">
         <div className="top-bar-left">
           <ul className="menu">
-            <li className="menu-text">React Menu App</li>
+            <li className="menu-text">React Weather App</li>
             <li>
               <IndexLink to='/' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
             </li>
@@ -27,7 +36,7 @@ var Nav = React.createClass ({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder="Search weather by city"/>
+                <input type="search" ref="search" placeholder="Search weather by city"/>
               </li>
               <li>
                 <input type="submit"  className="button" value="Get Weather"/>
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -15,21 +15,39 @@ var Weather = React.createClass({
 
     this.setState({
       isLoading: true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined,
     })
 
     openWeatherMap.getTemp(location).then(function (temp) {
       that.setState({
         location: location,
         temp: temp,
-        isLoading: false
+        isLoading: false,
       });
     }, function (e) {
       that.setState({
         isLoading: false,
-        errorMessage: e.message
+        errorMessage: e.message,
       });
     });
+  },
+  componentDidMount: function () {
+    var location = this.props.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = "#/";
+    }
+  },
+  componentWillReceiveProps: function (newProps){
+    var location = newProps.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = "#/";
+    }
   },
   render: function () {
     var {isLoading, temp, location, errorMessage} = this.state;
