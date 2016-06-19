7245149a79a8eeccbe6ccabef749ea0765adadd4
Joe Norris <yourboybstar@gmail.com>
2016-06-19 14:54:07 -0700

    Updated Styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>This is a simple Weather App I created to help develop my app-writing skills.  </p>
     <p>Here are some of the tools I used:</p>
     <ul>
